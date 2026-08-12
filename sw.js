cc847291446376447fdc97f3dd602cb34f00fa07
ken303867-png <ken303867@gmail.com>
2026-08-12 23:02:54 +0900

    Refresh PWA cache strategy for iPad Safari

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dysphagia-pwa-v2.1';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./app.part1'];
+const CACHE='dysphagia-pwa-v2.3';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r&&r.ok){const x=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,x));}return r;})));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',x));return r}).catch(()=>caches.match('./index.html')));return;}e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(c=>c||fetch(e.request).then(r=>{if(r&&r.ok){const x=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,x));}return r;})));});
