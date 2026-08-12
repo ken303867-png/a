839bfd6d6b71492bd4a016d032dd2f9c7d37a192
ken303867-png <ken303867@gmail.com>
2026-08-13 02:11:24 +0900

    Retire legacy root service worker

--- sw.js
+++ sw.js
@@ -1,65 +1,13 @@
-"use strict";
-
-const CACHE_NAME = "dysphagia-scale-drill-pwa-v3";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon.svg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", (event) => {
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
+      .then(keys=>Promise.all(keys.filter(k=>[
+        'dysphagia-pwa-v2.3',
+        'dysphagia-scale-drill-pwa-v2',
+        'dysphagia-scale-drill-pwa-v3'
+      ].includes(k)).map(k=>caches.delete(k))))
+      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.claim())
   );
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          if (response && response.ok) {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put("./index.html", copy));
-          }
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        if (response && response.status === 200) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      });
-    })
-  );
-});
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
